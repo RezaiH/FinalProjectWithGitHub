0dcca40490414afa986cbf3f7e89a01cb8b6dca4
Hussain <hussain.rasa111@gmail.com>
2022-11-12 12:20:43 -0500

    third commit

--- src/push/MyFirstClass.java
+++ src/push/MyFirstClass.java
@@ -2,4 +2,11 @@ package push;
 
 public class MyFirstClass {
 
+	public static void main(String[] args) {
+		System.out.println("Is this change pushed to the remote repository?");
+	}
+	
+	public static void isHeCollegeGraduate() {
+		System.out.println("Yea, he has graduated from George Town college!");
+	}
 }
