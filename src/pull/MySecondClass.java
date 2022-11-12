3cf2f8725897e34f78256ce9dbfee22102936b92
Hussain <hussain.rasa111@gmail.com>
2022-11-12 12:17:31 -0500

    first commit

--- src/pull/MySecondClass.java
+++ src/pull/MySecondClass.java
@@ -2,4 +2,10 @@ package pull;
 
 public class MySecondClass {
 
+	public static void howToPull() {
+		System.out.println("I want to pull the changes from the GitHub remote repository.");
+	}
+	public String giveYourAdd () {
+		return "his address is 1131 University Blvd, Silver Spring";
+	}
 }
